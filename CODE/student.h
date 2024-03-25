a9373050125312b2536f1aff788ad5804b65e315
erha riaz <erharandhawa@gmail.com>
2024-03-25 11:42:23 +0500

    editing student.h file

--- CODE/student.h
+++ CODE/student.h
@@ -5,8 +5,19 @@
 
 class Student
 {
+    private:
+
+    string StudentName;
+    string StudentID;
+    string StudentEnrollment;
+    string StudentSection;
+    string StudentDegree;
+    string StudentEmail;
     
-}
+    public:
+    Student(string name, string id, string enrollment, string section, string degree, string email):
+    StudentName(name), StudentID(id), StudentEnrollment(enrollment), StudentSection(section), StudentDegree(degree), StudentEmail(email) {}
+};
 
 #endif 
 
