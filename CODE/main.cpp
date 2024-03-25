3bab4cdfc4321c0361304e7414940b0dc6e4d32d
Abdul Rafay <rafay0820188@gmail.com>
2024-03-25 11:58:14 +0500

    creating main file

---
+++ CODE/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <limits>
+
+#include "Admin.h"	
+#include "Student.h"
+#include "Teacher.h"
+#include "Course.h"
+
+using namespace std;
+
+int main()
+{
+}
